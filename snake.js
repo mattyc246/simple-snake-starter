dbb7ed7e02fa7204ec8112516da286032d786918
Matthew Cross <mattyc246@gmail.com>
2020-03-26 15:08:47 +0800

    template finalized

--- snake.js
+++ snake.js
@@ -1,6 +1,16 @@
+/*
+################################################################
+################################################################
+##                          VARIABLES                         ##
+################################################################
+################################################################
+*/
+
 // Set the canvas border and background color
 const CANVAS_BORDER_COLOR = "black";
 const CANVAS_BACKGROUND_COLOR = "#FFFFFF";
+
+// Set the color for the snake body parts
 const SNAKE_COLOR = "limegreen";
 const SNAKE_BORDER_COLOR = "darkgreen";
 
@@ -13,10 +23,11 @@ let snake = [
   { x: 110, y: 150 }
 ];
 
-let score = 0;
-let speed = 150;
-let points = 5;
+let score = 0; // Score will keep track of the number of pieces of food eaten
+let speed = 150; // Speed will be used to determine how slow or fast the snake moves in milliseconds
+let points = 5; // This will be the value of each piece of food
 
+// Initialize changingDirection to false by default
 let changingDirection = false;
 
 // Horizontal Velocity
@@ -25,16 +36,28 @@ let dx = 15;
 // Vertical velocity
 let dy = 0;
 
+// Initialize food x axis position
 let foodX = 0;
 
+// Initialize food y axis position
 let foodY = 0;
 
 // Select the canvas that we will be working with
 const gameCanvas = document.getElementById("gameCanvas");
+
+// Select the scoreboard so we can update it each time food is eaten
 const scoreBoard = document.getElementById("score");
 
 // Return a 2 dimensions drawing context
 const ctx = gameCanvas.getContext("2d");
+
+/*
+################################################################
+################################################################
+##                   CANVAS FUNCTIONS                         ##
+################################################################
+################################################################
+*/
 
 // Function to set up the canvas
 function clearCanvas() {
@@ -44,6 +67,33 @@ function clearCanvas() {
   ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
   ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
+
+// Function to draw the snake
+function drawSnake() {
+  snake.forEach(function(snakePart) {
+    ctx.fillStyle = SNAKE_COLOR;
+    ctx.strokeStyle = SNAKE_BORDER_COLOR;
+
+    ctx.fillRect(snakePart.x, snakePart.y, 15, 15);
+    ctx.strokeRect(snakePart.x, snakePart.y, 15, 15);
+  });
+}
+
+// Function to draw the food
+function drawFood() {
+  ctx.fillStyle = "red";
+  ctx.strokeStyle = "darkred";
+  ctx.fillRect(foodX, foodY, 15, 15);
+  ctx.strokeRect(foodX, foodY, 15, 15);
+}
+
+/*
+################################################################
+################################################################
+##                         GAME LOGIC                         ##
+################################################################
+################################################################
+*/
 
 // Function to move the snake
 function advanceSnake() {
@@ -56,105 +106,20 @@ function advanceSnake() {
   if (didEatFood) {
     score += points;
     scoreBoard.innerHTML = score;
-    createFood();
   } else {
     snake.pop();
   }
 }
 
-// Function to draw the snake
-function drawSnakePart(snakePart) {
-  ctx.fillStyle = SNAKE_COLOR;
-  ctx.strokeStyle = SNAKE_BORDER_COLOR;
-
-  ctx.fillRect(snakePart.x, snakePart.y, 15, 15);
-  ctx.strokeRect(snakePart.x, snakePart.y, 15, 15);
-}
-
-function changeDirection(event) {
-  const LEFT_KEY = 37;
-  const RIGHT_KEY = 39;
-  const UP_KEY = 38;
-  const DOWN_KEY = 40;
-
-  if (changingDirection) return;
-
-  changingDirection = true;
-
-  const keyPressed = event.keyCode;
-  const goingUp = dy === -15;
-  const goingDown = dy === 15;
-  const goingRight = dx === 15;
-  const goingLeft = dx === -15;
-
-  if (keyPressed === LEFT_KEY && !goingRight) {
-    dx = -15;
-    dy = 0;
-  }
-
-  if (keyPressed === UP_KEY && !goingDown) {
-    dx = 0;
-    dy = -15;
-  }
-
-  if (keyPressed === RIGHT_KEY && !goingLeft) {
-    dx = 15;
-    dy = 0;
-  }
-
-  if (keyPressed === DOWN_KEY && !goingUp) {
-    dx = 0;
-    dy = 15;
-  }
-}
-
-function drawSnake() {
-  snake.forEach(drawSnakePart);
-}
-
+// Function to create random x & y axis position for generating snake
 function randomTen(min, max) {
   return Math.round((Math.random() * (max - min) + min) / 15) * 15;
 }
 
-function createFood() {
-  foodX = randomTen(0, gameCanvas.width - 15);
-  foodY = randomTen(0, gameCanvas.height - 15);
-
-  snake.forEach(function isFoodOnSnake(part) {
-    const foodIsOnSnake = part.x == foodX && part.y == foodY;
-    if (foodIsOnSnake) {
-      createFood();
-    }
-  });
-}
-
-function drawFood() {
-  ctx.fillStyle = "red";
-  ctx.strokeStyle = "darkred";
-  ctx.fillRect(foodX, foodY, 15, 15);
-  ctx.strokeRect(foodX, foodY, 15, 15);
-}
-
-function didGameEnd() {
-  for (let i = 4; i < snake.length; i++) {
-    const didCollide = snake[i].x === snake[0].x && snake[i].y === snake[0].y;
-    if (didCollide) return true;
-  }
-
-  const hitLeftWall = snake[0].x < 0;
-  const hitRightWall = snake[0].x > gameCanvas.width - 15;
-  const hitTopWall = snake[0].y < 0;
-  const hitBottomWall = snake[0].y > gameCanvas.height - 15;
-
-  return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall;
-}
-
+// Function to start the game and play
 function play() {
-  if (didGameEnd()) {
-    alert("Game Over");
-    return;
-  }
-  setTimeout(function onTick() {
+
+  setTimeout(function() {
     changingDirection = false;
     clearCanvas();
     drawFood();
@@ -165,9 +130,15 @@ function play() {
   }, speed);
 }
 
-document.addEventListener("keydown", changeDirection);
+/*
+################################################################
+################################################################
+##                        DRIVER CODE                         ##
+################################################################
+################################################################
+*/
 
 alert("Click OK to start playing!");
-createFood();
+drawFood();
 play();
 clearCanvas();
